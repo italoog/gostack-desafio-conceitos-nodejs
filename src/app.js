9f4b704d4704ef6f166185b6edb1b661dd0e3219
Ítalo Gomes <47856024+italoog@users.noreply.github.com>
2020-11-18 11:29:02 -0300

    Conclução desafio
    Desafio GoStack comoceitos Node.js

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const { title, url, techs} = request.body;
+  const { title, url, techs } = request.body;
 
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
@@ -46,6 +46,7 @@ app.put("/repositories/:id", (request, response) => {
     title,
     url,
     techs,
+    likes: repositories[repositoryIndex].likes,
   };
 
   repositories[repositoryIndex]=repository;
@@ -68,7 +69,17 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'repository not found.'})
+  }
+
+  repositories[repositoryIndex].likes++
+
+  return response.json(repositories[repositoryIndex]);  
 });
 
 module.exports = app;
